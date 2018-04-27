faf3cf8c92290b781230b0166331be5598d48d6a
UnknownState <35842847+UnknownState@users.noreply.github.com>
2018-04-27 19:48:49 -0400

    Added a basic graphic for tiles. Created a tile component and grid component. Added basic random spawning of a empty tile prefab to the grid.

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour {
+
+    #region Public
+    public GameObject EmptyTilePrefab;
+    public int MaxDefaultSpawnCount = 3;
+    [Space]
+    public List<GameObject> EmptyTiles;
+
+    #endregion
+    #region Private
+	
+    #endregion
+
+    #region Unity Callback
+    void Start ()
+    {
+        
+        int randomSpawnCount = Random.Range(1, MaxDefaultSpawnCount);
+
+        List<GameObject> spawnPoints = new List<GameObject>();
+        int previousSpawnIndex = -1;
+
+        while (spawnPoints.Count < randomSpawnCount)
+        {
+            //Get a random index to spawn the tile at
+            int randomSpawnIndex = Random.Range(0, EmptyTiles.Count - 1);
+            //Check if the spawn index has already been used
+            if (randomSpawnIndex == previousSpawnIndex)
+                continue;
+            //Set the previous spawn index to keep duplicate tiles from spawning
+            previousSpawnIndex = randomSpawnIndex;
+            //Add a new spawn point
+            spawnPoints.Add(EmptyTiles[randomSpawnIndex]);
+        }
+
+        foreach (var t in spawnPoints)
+        {
+            Instantiate(EmptyTilePrefab, t.transform);
+        }
+    }
+
+    #endregion
+    #region Methods
+	
+    #endregion
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Tile : MonoBehaviour
+{
+    public GameObject TileObject;
+    public Image TileImage;
+    public Text TileText;
+
+    public uint TileValue;
+    public Color Color;
+
+}
